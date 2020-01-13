b29ca97f902b3dac97c807fc3b65dfda52caff7d
Pascal Simplice <pascal@ayoupa.com>
2020-01-13 15:17:42 +0000

    Added GTM pod files

---
+++ GTMSupport/GTMSupport.swift
@@ -0,0 +1,11 @@
+//
+//  GTMSupport.swift
+//  GTMSupport
+//
+//  Created by Pascal Simplice on 13/01/2020.
+//  Copyright © 2020 Pascal Simplice. All rights reserved.
+//
+import FirebaseAnalytics
+
+class GTMSupport {
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+
+# Uncomment the next line to define a global platform for your project
+target 'GTMSupport' do
+  platform :ios, '9.1'
+  pod 'GoogleTagManager', '~> 6.0'
+end
