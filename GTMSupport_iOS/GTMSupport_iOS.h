10b1340214419f713df56da556438f5042c48307
Pascal Simplice <pascal@ayoupa.com>
2020-01-13 15:40:31 +0000

    Added iOS scheme

---
+++ GTMSupport_iOS/GTMSupport_iOS.h
@@ -0,0 +1,19 @@
+//
+//  GTMSupport_iOS.h
+//  GTMSupport_iOS
+//
+//  Created by Pascal Simplice on 13/01/2020.
+//  Copyright © 2020 Pascal Simplice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTMSupport_iOS.
+FOUNDATION_EXPORT double GTMSupport_iOSVersionNumber;
+
+//! Project version string for GTMSupport_iOS.
+FOUNDATION_EXPORT const unsigned char GTMSupport_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTMSupport_iOS/PublicHeader.h>
+
+
