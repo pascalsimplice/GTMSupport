1dce3aa9b79300207f295479778490885f156ce7
Pascal Simplice <pascal@ayoupa.com>
2020-01-13 15:46:05 +0000

    Added public header with firebase

--- GTMSupport_iOS/GTMSupport_iOS.h
+++ GTMSupport_iOS/GTMSupport_iOS.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char GTMSupport_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GTMSupport_iOS/PublicHeader.h>
 
-
+#import <GTMSupport_iOS/PublicHeader.h>
---
+++ GTMSupport_iOS/PublicHeader.h
@@ -0,0 +1,15 @@
+//
+//  PublicHeader.h
+//  GTMSupport
+//
+//  Created by Pascal Simplice on 13/01/2020.
+//  Copyright © 2020 Pascal Simplice. All rights reserved.
+//
+
+#ifndef PublicHeader_h
+#define PublicHeader_h
+
+#import <FirebaseCore/FirebaseCore.h>
+#import <GoogleTagManager/GoogleTagManager.h>
+
+#endif /* PublicHeader_h */
