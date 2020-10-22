256b40c853ae0cb19d8da5e59ffbb08cce394b67
Pascal Simplice <pascal.simplice@deltatre.com>
2020-10-22 11:37:39 +0100

    Added TestView

---
+++ GTMSupport_tvOS/TestView.swift
@@ -0,0 +1,22 @@
+//
+//  TestView.swift
+//  GTMSupport_tvOS
+//
+//  Created by Pascal Simplice on 22/10/2020.
+//  Copyright © 2020 Pascal Simplice. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class TestView: UIView {
+
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+
+}
